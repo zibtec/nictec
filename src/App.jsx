85d203982361627fb10d51cd88bef96720dbcb0e
Nick <zibdevsec@gmail.com>
2026-04-20 12:59:11 -0700

    Add About page and update app routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import NickCouryPortfolioHomepage from './NickCouryPortfolioHomepage'
+import About from '../About'
 
 function App() {
-  return <NickCouryPortfolioHomepage />
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<NickCouryPortfolioHomepage />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </Router>
+  )
 }
 
 export default App
