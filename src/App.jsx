5259defcad7537a4418f555d8594616e6aedd7c9
Nick <zibdevsec@gmail.com>
2026-04-28 18:38:27 -0700

    update about page

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,6 @@ import Blog from "./pages/Blog";
 import ExperiencePage from "./pages/Experience";
 import InitiativesPage from "./pages/Initiatives";
 import EducationPage from "./pages/Education";
-import CoursesCompletedPage from "./pages/CoursesCompleted";
 
 function CloudflareAnalytics() {
   React.useEffect(() => {
@@ -80,7 +79,6 @@ function App() {
         />
         <Route path="/blog" element={<Blog />} />
         <Route path="/about" element={<About />} />
-        <Route path="/about/courses-completed" element={<CoursesCompletedPage />} />
         <Route path="/experience" element={<ExperiencePage />} />
         <Route path="/initiatives" element={<InitiativesPage />} />
         <Route path="/education" element={<EducationPage />} />
