5fd4e48160548c719263c4677915d5948af7d73b
NickC <ncoury@wgu.edu>
2026-05-13 17:29:31 -0700

    Fixed errors

--- src/data/caseStudies-data.js
+++ src/data/caseStudies-data.js
@@ -61,5 +61,6 @@ const caseStudiesData = [
     ],
   result:
     "Building a strong foundation in modern cybersecurity governance, infrastructure security, and compliance-aware operations through continuous learning, practical implementation, and ongoing exposure to enterprise-aligned security frameworks and best practices."
-}
+  },
+];
 export default caseStudiesData;
